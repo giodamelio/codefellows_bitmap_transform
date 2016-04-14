553e2030af421c8ca83edd06556497604527acca
Gio d'Amelio <giodamelio@gmail.com>
2016-04-14 16:54:26 -0700

    Parse bitmap size

--- src/parse.js
+++ src/parse.js
@@ -1,8 +1,16 @@
 import { Buffer } from 'buffer';
 
 export default function parse(data) {
+  // The output data
+  const output = {};
+
   // Check for a valid bitmap header
   if (data.slice(0, 2).compare(new Buffer([0x42, 0x4d])) !== 0) {
     throw new Error('Invalid bitmap header');
   }
+
+  // Get the size of the bmp file
+  output.size = data.readInt32LE(2);
+
+  return output;
 }
---
+++ test/index.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+
+import parseBitmap from '../src/parse';
+
+// Load some example data
+const image1 = fs.readFileSync(path.join(__dirname, 'image1.bmp'));
+const invalidHeader = fs.readFileSync(path.join(__dirname, 'invalidHeader.bmp'));
+
+describe('Parser', () => {
+  describe('Header', () => {
+    it('should parse valid header', () => {
+      (() => {
+        parseBitmap(image1);
+      }).should.not.throw();
+    });
+
+    it('should error on invalid header', () => {
+      (() => {
+        parseBitmap(invalidHeader);
+      }).should.throw('Invalid bitmap header');
+    });
+
+    it('should read the image size in bytes', () => {
+      const image = parseBitmap(image1);
+      image.size.should.equal(786554);
+    });
+  });
+});
