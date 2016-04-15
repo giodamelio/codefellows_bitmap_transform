fd55899cc911d76d4cf977a41b05711dfeaafdc6
Gio d'Amelio <giodamelio@gmail.com>
2016-04-14 19:11:49 -0700

    Refactor to allow for easy transform creation

--- src/index.js
+++ src/index.js
@@ -1,37 +1,58 @@
 import fs from 'fs';
 import path from 'path';
+import { Buffer } from 'buffer';
 
 import parseBitmap from './parse';
 
-// Read the file
-const imagePath = path.join(__dirname, '../test/images/image1.bmp');
-fs.readFile(imagePath, (readErr, image) => {
-  if (readErr) {
-    console.error(readErr);
-    process.exit(1);
-  }
+// Clone a buffer
+function cloneBuffer(oldBuffer) {
+  const newBuffer = Buffer.alloc(oldBuffer.length);
+  oldBuffer.copy(newBuffer);
+  return newBuffer;
+}
 
-  try {
-    // Parse the file
-    const data = parseBitmap(image);
-
-    // Invert the colors
-    // Since the image.rawPixels points to the same memory as originalImage
-    // it can be modified in place
-    for (let index = 0; index < data.rawPixels.length; index++) {
-      data.rawPixels[index] = ~data.rawPixels[index];
+// Run a transform on an image
+function transform(inputPath, outputPath, transformCallback) {
+  // Read the file
+  fs.readFile(inputPath, (readErr, image) => {
+    if (readErr) {
+      console.error(readErr);
+      process.exit(1);
     }
 
+    // Parse the file
+    let originalImageData;
+    try {
+      originalImageData = parseBitmap(image);
+    } catch (parseError) {
+      throw parseError;
+    }
+
+    // Copy the pixel data so we don't clobber the original
+    const transformedPixels = cloneBuffer(originalImageData.rawPixels);
+
+    // Run the transform
+    transformCallback(transformedPixels);
+
+    // Copy the transformed pixels back into the original image
+    transformedPixels.copy(image, originalImageData.pixelArrayOffset);
+
     // Write the modified image back to a new file
-    const outputPath = path.join(__dirname, '../output.bmp');
     fs.writeFile(outputPath, image, (err) => {
       if (err) {
         throw err;
       }
 
-      console.log(`Output image written to ${outputPath}`);
+      console.log(`Image written to ${outputPath}`);
     });
-  } catch (err) {
-    console.error(err);
+  });
+}
+
+const imagePath = path.join(__dirname, '../test/images/image1.bmp');
+
+// Invert the image
+transform(imagePath, 'inverted.bmp', (data) => {
+  for (let index = 0; index < data.length; index++) {
+    data[index] = ~data[index];
   }
 });
