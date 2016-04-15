f5db94d18904d5625c5bc4e2b90297d5344830e8
Gio d'Amelio <giodamelio@gmail.com>
2016-04-14 18:40:15 -0700

    Invert pixels

--- src/index.js
+++ src/index.js
@@ -4,15 +4,33 @@ import path from 'path';
 import parseBitmap from './parse';
 
 // Read the file
-fs.readFile(path.join(__dirname, '../test/image1.bmp'), (err, data) => {
-  if (err) {
-    console.error(err);
+const imagePath = path.join(__dirname, '../test/images/image1.bmp');
+fs.readFile(imagePath, (readErr, image) => {
+  if (readErr) {
+    console.error(readErr);
     process.exit(1);
   }
 
   try {
-    const file = parseBitmap(data);
-    console.log(file);
+    // Parse the file
+    const data = parseBitmap(image);
+
+    // Invert the colors
+    // Since the image.rawPixels points to the same memory as originalImage
+    // it can be modified in place
+    for (let index = 0; index < data.rawPixels.length; index++) {
+      data.rawPixels[index] = ~data.rawPixels[index];
+    }
+
+    // Write the modified image back to a new file
+    const outputPath = path.join(__dirname, '../output.bmp');
+    fs.writeFile(outputPath, image, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      console.log(`Output image written to ${outputPath}`);
+    });
   } catch (err) {
     console.error(err);
   }
